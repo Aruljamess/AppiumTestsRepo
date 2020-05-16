a3f98ba460157596fa11592343ee50b9af2f26f7
God <God@192.168.0.4>
2020-05-17 00:55:09 +0200

    Successful run on calculator app

--- AppiumTests/src/test/java/tests/AppiumTest.java
+++ AppiumTests/src/test/java/tests/AppiumTest.java
@@ -1,5 +1,6 @@
 package tests;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -18,38 +19,41 @@ public class AppiumTest
 			openCalculator();
 		} catch (Exception e)
 		{
-			System.out.println(e.getCause());
-			System.out.println(e.getMessage());
+			System.out.println("GetCaues: "+e.getCause());
+			System.out.println("GetMessage:"+e.getMessage());
 			e.printStackTrace();
 		}
 	}
 
 	public static void openCalculator() throws Exception
 	{
+		System.out.println("DesiredCapabilities setup");
 		DesiredCapabilities cap = new DesiredCapabilities();
-
 		cap.setCapability("deviceName", "vivo 1716");
-		cap.setCapability("udid", "46567834");
+		cap.setCapability("udid", "5689c21a");
 		cap.setCapability("platformName", "Android");
 		cap.setCapability("platformVersion", "8.1.0");
 		cap.setCapability("appPackage", "com.android.bbkcalculator");
 		cap.setCapability("appActivity", "com.android.bbkcalculator.Calculator");
 
-		URL url = new URL("http://127.0.0.0.1:4723/wd/hub"); // URL of appium server
+		URL url = new URL("http://0.0.0.0:4723/wd/hub"); // URL of appium server
 		driver = new AppiumDriver<MobileElement>(url, cap);
 		System.out.println("Application Started.....");
-
-		MobileElement two = driver.findElement(By.id("com.myos.calculator2:id/digit2"));
-		MobileElement plus= driver.findElement(By.id("com.myos.calculator2:id/plus"));
-		MobileElement three= driver.findElement(By.id("com.myos.calculator2:id/digit3"));
-		MobileElement equals = driver.findElement(By.id("com.myos.calculator2:id/equals"));
-		// to verify result press 2 , tab, press +, tab, press 3 , tab, press = , tab,  select result 5, copy class name attribute and paste 
-		MobileElement results = driver.findElement(By.className("android.widget.EditText"));
-
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		// use uiautomatorviewer to find element id
+		MobileElement two = driver.findElement(By.id("com.android.bbkcalculator:id/digit2"));
+		MobileElement plus= driver.findElement(By.id("com.android.bbkcalculator:id/plus"));
+		MobileElement three= driver.findElement(By.id("com.android.bbkcalculator:id/digit3"));
+		MobileElement equals = driver.findElement(By.id("com.android.bbkcalculator:id/equal"));
+		
 		two.click();
 		plus.click();
 		three.click();
 		equals .click();
+		
+		// to verify result press 2 , tab, press +, tab, press 3 , tab, press = , tab,  select result 5, copy class name attribute and paste 
+		MobileElement results = driver.findElement(By.className("com.android.bbkcalculator:id/input_edit"));
+				
 		String res = results.getText();
 		System.out.println("Results is :" +res);
 	}
